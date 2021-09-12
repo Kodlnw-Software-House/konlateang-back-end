2d7b98fc8c5fc45e18c7711e57e7999a4cdf26d5
UntillNIGHT <lengleng9090@gmail.com>
2021-09-12 21:43:22 +0700

    [UPDATE] reduce boilerplate code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,133 +3,65 @@ const {Admin,AdminToken} = require('../models/adminstator')
 const {Hostipal,HospitalToken} = require('../models/hospital')
 const {Patient,PatientToken} = require('../models/patient')
 
-const adminAuth = async (req,res,next)=>{
-    try{
-    const token = req.header('Authorization').replace('Bearer ','')
-    const isMatch = await AdminToken.findOne({where:{token}})
-
-    if(!isMatch){
-        throw new Error();
-    }
-
-    const decoded = jwt.verify(token,process.env.JWTSECRET)
-    const adminResult = await Admin.findOne({where:{email:decoded.email}})
-
-    if(!adminResult){
-        throw new Error();
-    }
-
-    req.token = token;
-    req.admin = adminResult;
-
-    next();
-
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
-    }
-}
-
-const hospitalAuth = async (req,res,next)=>{
-    try{
+const auth = function(role,tokenType){
+    return async function (req, res, next){
+        try{
         const token = req.header('Authorization').replace('Bearer ','')
-        const isMatch = await HospitalToken.findOne({where:{token}})
 
-        if(!isMatch){
+        let decoded
+        if(tokenType==='REFRESH'){
+            decoded = jwt.verify(token,process.env.JWTSECRET)
+        }
+        else if(tokenType==='ACCESS'){
+            decoded = jwt.verify(token,process.env.JWTACCESSTOKEN);
+        }
+        
+        let isMatch
+        if(role==='ADMIN'){
+            isMatch = await AdminToken.findOne({where:{token}})
+            if(!isMatch){
+                throw new Error();
+            }
+            const adminResult = await Admin.findOne({where:{email:decoded.email}})
+            if(!adminResult){
+                throw new Error();
+            }
+            req.admin = adminResult
+        }
+        else if(role==='HOSPITAL'){
+            isMatch = await HospitalToken.findOne({where:{token}})
+            if(!isMatch){
+                throw new Error();
+            }
+            const hospitalResult = await Hostipal.findOne({where:{email:decoded.email}})
+            if(!hospitalResult){
+                throw new Error();
+            }
+            req.hospital = hospitalResult
+        }
+        else if(role==='PATIENT'){
+            isMatch = await PatientToken.findOne({where:{token}});
+            if(!isMatch){
+                throw new Error();
+            }
+            const patientResult = await Patient.findOne({where:{email:decoded.email}})
+            if(!patientResult){
+                throw new Error();
+            }
+            req.patient = patientResult
+        }else{
             throw new Error();
         }
 
-        const decoded = jwt.verify(token,process.env.JWTSECRET);
-        const hospitalResult = await Hostipal.findOne({where:{email:decoded.email}})
-
-        if(!hospitalResult){
-            throw new Error();
-        }
-
-        req.token = token;
-        req.hospital = hospitalResult;
+        req.token = token
 
         next();
-
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
-    }
-}
-
-const patientAuth = async (req,res,next)=>{
-    try{
-        const token = req.header('Authorization').replace('Bearer ','')
-        const isMatch = await PatientToken.findOne({where:{token}});
-        if(!isMatch){
-            throw new Error();
+        }catch(error){
+            res.status(401).send({error:'Please authenticate.'})
         }
-        const decoded = jwt.verify(token,process.env.JWTSECRET);
-        const patientResult = await Patient.findOne({where:{email:decoded.email}});
-        if(!patientResult){
-            throw new Error();
-        }
-
-        req.token = token;
-        req.patient = patientResult;
-
-        next();
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
-    }
-}
-
-const adminAccessAuth = async (req,res,next) =>{
-    try{
-        const accessToken = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(accessToken,process.env.JWTACCESSTOKEN);
-        const adminResult = await Admin.findOne({where:{email:decoded.email}})
-        if(!adminResult){
-            throw new Error();
-        }
-        req.admin = adminResult;
-
-        next();
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
-    }
-}
-
-const hospitalAccessAuth = async(req,res,next) =>{
-    try{
-        const accessToken = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(accessToken,process.env.JWTACCESSTOKEN);
-        const hospitalResult = await Hostipal.findOne({where:{email:decoded.email}})
-        if(!hospitalResult){
-            throw new Error();
-        }
-        req.hospital = hospitalResult;
-
-        next();
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
-    }
-}
-
-const patientAccessAuth = async(req,res,next)=>{
-    try{
-        const accessToken = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(accessToken,process.env.JWTACCESSTOKEN);
-        const patientResult = await Patient.findOne({where:{email:decoded.email}})
-        if(!patientResult){
-            throw new Error();
-        }
-        req.patient = patientResult;
-
-        next();
-    }catch(error){
-        res.status(401).send({error:'Please authenticate.'})
     }
 }
 
 module.exports = {
-    adminAuth,
-    hospitalAuth,
-    patientAuth,
-    adminAccessAuth,
-    hospitalAccessAuth,
-    patientAccessAuth
+    auth
 }
