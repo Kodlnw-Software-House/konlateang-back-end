b0b0bb7e70bd06690281ae736e95c3424cdf8e68
UntillNIGHT <lengleng9090@gmail.com>
2021-10-09 22:07:11 +0700

    [ADD] new endpoint /me /getisolation and /edit/:id

--- src/routers/hospital.js
+++ src/routers/hospital.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const multer = require('multer')
 
 const router = new express.Router();
 
+const upload = multer()
+
 const {Hostipal,HospitalToken} = require('../models/hospital')
-const {auth} = require('../middleware/auth')
+const {auth} = require('../middleware/auth');
+const { Isolation } = require('../models/isolation');
 
 router.get('/getall',async (req,res)=>{
     try{
@@ -27,6 +31,29 @@ router.post('/login',async (req,res)=>{
     }catch(error){
         res.status(400).send({error:error.message});
     }
+})
+
+router.get('/me',auth('HOSPITAL'),async(req,res)=>{
+    res.status(200).send({hospital:req.hospital})
+})
+
+router.get('/getIsolations',auth('HOSPITAL'),async(req,res)=>{
+    const isolation = await Isolation.findAll({where:{
+        hospital_id: req.hospital.hospital_id
+    }})
+    res.status(200).send({isolation})
+})
+
+router.put('/edit/:id',upload.array(),auth('HOSPITAL'),async(req,res)=>{
+    try{
+        await Isolation.update(req.body,{
+            where:{community_isolation_id:req.params.id}
+        })
+        res.status(200).send({status:'update successful.'})
+    }catch(error){
+        res.status(500).send({error:error.message})
+    }
+    
 })
 
 router.delete('/logout', auth('HOSPITAL'),async (req,res)=>{
