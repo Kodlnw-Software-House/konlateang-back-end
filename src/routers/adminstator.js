b55c98d200b67c7db6ee3b4f518cc9f8b785a37a
UntillNIGHT <lengleng9090@gmail.com>
2021-11-16 20:21:11 +0700

    [ADD] getAllIsolation

--- src/routers/adminstator.js
+++ src/routers/adminstator.js
@@ -9,6 +9,9 @@ const upload = multer()
 const {Admin,AdminToken} = require('../models/adminstator')
 const {Patient} = require('../models/patient')
 const {auth} = require('../middleware/auth')
+const {Isolation,IsolationImage} = require('../models/isolation')
+const {Hostipal} = require('../models/hospital')
+const Booking = require('../models/booking')
 
 router.post('/login',upload.array(),async (req,res)=>{
     try{
@@ -106,5 +109,65 @@ router.put('/editPatient/:id',auth('ADMIN'),async(req,res)=>{
     }
 })
 
+router.get('/getAllIsolation',auth('ADMIN'),async(req,res)=>{
+    req.query.pageNumber = !req.query.pageNumber ? 1 : req.query.pageNumber
+    const search = req.query.search ? req.query.search : ''
+    const limit = parseInt(req.query.pageSize)
+    const offset = limit * (parseInt(req.query.pageNumber)-1)
+    Isolation.findAndCountAll({
+        where:{
+            [Op.or]:[{
+                community_isolation_name:{
+                    [Op.substring]: search
+                }
+            },
+            {
+                address:{
+                    [Op.substring] : search
+                }
+            }
+        ]
+        },
+        include:[{
+            model: Hostipal,
+            as:'Hospital',
+            attributes:{
+                exclude: ['password']
+            }
+        }],
+        attributes:{
+            exclude: ['hospital_id']
+        },
+        limit,
+        offset,
+    }).then(async (result)=>{
+        for (let i=0;i<result.rows.length;i++) {
+            const bookingLeft = await Booking.count({where:
+                {
+                    community_isolation_id: result.rows[i].community_isolation_id,
+                    status_id: {
+                        [Op.or]:[2,4]
+                    }
+                }
+            })
+            result.rows[i].dataValues.bed_left = result.rows[i].available_bed - bookingLeft;
+
+            const imageIndex = await IsolationImage.findAll({where:
+                {
+                    community_isolation_id: result.rows[i].community_isolation_id
+                },
+                attributes:{
+                    exclude: ['image_id','image','community_isolation_id']
+                },
+                order:[['index', 'ASC']]
+            })
+            result.rows[i].dataValues.image_index = imageIndex.map(u => u.get("index"));
+        }
+        result.totalPage = Math.ceil(result.count / limit)
+        res.status(200).send({result})
+    }).catch((error)=>{
+        res.status(500).send({error:error.message})
+    })
+})
 
 module.exports = router;
